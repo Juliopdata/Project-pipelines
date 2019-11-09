e7549b61c5eb13cbf99e1ac3990910d14c33d203
Julio Porto <juliopdata@gmail.com>
2019-11-09 19:51:52 +0100

    Limpieza casi terminada

--- dataset.py
+++ dataset.py
@@ -1,1 +1,4 @@
 import pandas as pd
+import re
+df = pd.read_csv('./src/movies_metadata.csv')
+dfclean = df.drop(['popularity', 'id', 'spoken_languages', 'tagline', 'budget', 'vote_count', 'belongs_to_collection', 'homepage', 'imdb_id', 'production_companies', 'production_countries', 'revenue', 'status', 'video'], axis=1)
