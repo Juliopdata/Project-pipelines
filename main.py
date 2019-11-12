51e4892808cf99ef19cb19de114074279c7f3723
Julio Porto <juliopdata@gmail.com>
2019-11-12 10:12:42 +0100

    pruebas finales con el argparse

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import subprocess
 def parse():
 	parser = argparse.ArgumentParser(description='Choose a Genre, get a recommendation in your email')
 	parser.add_argument('genre', help='Choose: comedy, romance, drama, horror or thriller', default='Horror')           # action guarda el argumento
-	parser.add_argument("mail", help="Mail will receive the recommendation")
+	parser.add_argument('mail', help="Mail will receive the recommendation")
 	args = parser.parse_args()
 	print(args)
 	return args
@@ -21,8 +21,6 @@ def parse():
 
 def main():
 	args = parse()
-	
-
 	print(sys.argv)
 	genre = args.genre
 	email = args.mail
@@ -31,14 +29,12 @@ def main():
 	moviereport = showMovie(movie)
 	pdf = createPDF(moviereport)
 	print('You have a movie recommendation report')
-	
 	if args.mail != None:
 		mail = checkMail(args.mail)
 		sendMail(mail,pdf)
 		print ("The movie recommendation has been sent to {}".format(email))
 	else:
 		print('You have the report in the path: {}'.format(moviereport[0]))
-
 	text = '"DING, DONG, YOU HAVE AN EMAIL"'
 	subprocess.call('espeak -vF4 '+text, shell=True)
 
